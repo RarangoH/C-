5cda8217d6e49279173fb8d7d25b43aaf5ee5ece
RarangoH <rafaarangoh@gmail.com>
2024-12-04 09:48:43 -0500

    first commit

---
+++ TodoList/TodoList/TodoList/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+
+
+//Variable Declaration
+int number;
+
+//Variable Initialization
+number = 5;
+
+//Variable Initialization at declaration
+string text =  "abc";
+
+//Declaring Multiple Variables at Same Time
+string name, lastname;
+int a = 1, b = 2;
+
+Console.ReadKey();
